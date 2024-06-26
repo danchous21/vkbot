abf816d72c86696e6e69cf4f08bf92fac0ab5009
danchous21 <danceussisk228@gmail.com>
2024-06-26 19:09:28 +0300

    Добавил логику бота, сценарии ответа, запрашивает у пользователя имя и почту. Добавил модуль handlers, который обрабатывет сообщение пользователя с помощью регулярных выражений

--- vkbot/bot.py
+++ vkbot/bot.py
@@ -2,11 +2,12 @@ import logging
 import random
 
 import vk_api
+from vkbot import handlers
 
 try:
-    from settings import TOKEN, GROUP_ID
+    import vkbot.settings as settings
 except ImportError:
-    exit('Необходимо создать файл settings.py с параметрами TOKEN и GROUP_ID')
+    exit('Необходимо создать файл settings.py с параметрами TOKEN, GROUP_ID, INTENTS')
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 log = logging.getLogger('Bot')
@@ -25,50 +26,109 @@ def configure_logging():
     log.setLevel(logging.DEBUG)
 
 
+class UserState:
+    '''
+    Состояние пользователя внутри сценария
+    '''
+
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
+
+
 class Bot:
     """
+    Сценарий регистрации на конференцию через Vk.com.
+
+    Поддерживает ответы на вопросы про дату, место проведения и сценарий регистрации:
+    - спрашивает имя
+    - спрашивает email
+    - говорим об успешной регистрации
+    Если шаг не пройден, задаем вопрос пока шаг не будет пройден.
     Echo bot для vk.com
     Use python 3.12
     """
 
-    def __init__(self, GROUP_ID, TOKEN):
+    def __init__(self, group_id, token):
         """
-        :param GROUP_ID: group id из группы vk
-        :param TOKEN: секретный токен
+        :param group_id: group id из группы vk
+        :param token: секретный токен
         """
-        self.group_id = GROUP_ID
-        self.token = TOKEN
-        self.vk = vk_api.VkApi(token=TOKEN)
+        self.group_id = group_id
+        self.token = token
+        self.vk = vk_api.VkApi(token=token)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
         self.api = self.vk.get_api()
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         for event in self.long_poller.listen():
             """Запуск Бота"""
             try:
                 log.debug('Получено событие: %s', event)
-                self.on_event(event)
+                self.on_event(event, settings)
             except Exception:
                 log.exception('Ошибка в обработке события')
 
-    def on_event(self, event: VkBotEventType):
+    def on_event(self, event, settings):
         """Отправляет сообщение назад, если это текст
 
         :param event: VkBotMessageEvent Object
+        :param settings: настройки бота
         :return: None
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            log.debug('Отправляем сообщение назад')
-            self.api.messages.send(
-                message=event.object.get('message', {}).get('text'),
-                random_id=random.randint(0, 2 ** 20),
-                peer_id=event.object['message']['peer_id'],
-            )
-        else:
+        if event.type != VkBotEventType.MESSAGE_NEW:
             log.info('Мы пока не умеем обрабатывать событие такого типа %s', event.type)
+            return
+
+        user_id = event.object['message']['peer_id']
+        text = event.object.get('message', {}).get('text')
+
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id, text)
+        else:
+            for intent in settings.INTENTS:
+                if any(token in text for token in intent['tokens']):
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=user_id,
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
+
+    def continue_scenario(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+        handler = getattr(handlers, step['handler'])
+        if handler(text, context=state.context):
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                state.step_name = step['next_step']
+            else:
+                self.user_states.pop(user_id)
+        else:
+            text_to_send = step['failure_text'].format(**state.context)
+        return text_to_send
 
 
 if __name__ == '__main__':
     configure_logging()
-    bot = Bot(GROUP_ID=GROUP_ID, TOKEN=TOKEN)
+    bot = Bot(group_id=settings.GROUP_ID, token=settings.TOKEN)
     bot.run()
